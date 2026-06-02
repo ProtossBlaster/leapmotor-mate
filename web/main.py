a19fc539e4a89a9f3aa1c1983ea913e007951667
Corrado Gamberoni <hubcasale@hubcasale>
2026-06-03 00:07:56 +0200

    Fix NameError: re-add BackgroundTasks import

--- web/main.py
+++ web/main.py
@@ -4,7 +4,7 @@ import sys
 import time
 from pathlib import Path
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, BackgroundTasks
 from fastapi.responses import HTMLResponse, RedirectResponse, Response, FileResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
